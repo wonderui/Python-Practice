33a2a6b21f9b01acd8b021ae49893848d884f6ab
wonderui <wonderui@qq.com>
2017-03-27 19:01:56 +0800

    170327

---
+++ Probability-Practice/MontyHall.py
@@ -0,0 +1,24 @@
+import random
+
+# 关于random库的小测试
+'''
+a = [1, 2, 3]
+n = 1000
+a1 = 0
+a2 = 0
+a3 = 0
+
+for i in range(n):
+  r = random.sample(a, 1)
+  if r == [1]:
+        a1 = a1 + 1
+  elif r == [2]:
+        a2 = a2 + 1
+  elif r == [3]:
+        a3 = a3 + 1
+
+print float(a1)/float(n)
+print float(a2)/float(n)
+print float(a3)/float(n)
+'''
+
