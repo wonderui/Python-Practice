27f72e163297319bdbfd56ffd63bcbdf8c848f86
wonderui <wonderui@qq.com>
2017-05-15 18:50:29 +0800

    PlainTextMessage

--- Courses/6.00.1x/ProblemSet6/ps6.py
+++ Courses/6.00.1x/ProblemSet6/ps6.py
@@ -150,15 +150,19 @@ class PlaintextMessage(Message):
         Hint: consider using the parent class constructor so less 
         code is repeated
         '''
-        pass #delete this line and replace with your code here
-
+        Message.__init__(self, text)  #initial Message attributes
+        self.shift = shift
+        self.encrypting_dict = Message.build_shift_dict(self, shift)
+        self.message_text_encrypted = Message.apply_shift(self, shift)
+        
+        
     def get_shift(self):
         '''
         Used to safely access self.shift outside of the class
         
         Returns: self.shift
         '''
-        pass #delete this line and replace with your code here
+        return self.shift
 
     def get_encrypting_dict(self):
         '''
@@ -166,7 +170,8 @@ class PlaintextMessage(Message):
         
         Returns: a COPY of self.encrypting_dict
         '''
-        pass #delete this line and replace with your code here
+        copy_dict = self.encrypting_dict.copy()
+        return copy_dict
 
     def get_message_text_encrypted(self):
         '''
@@ -174,7 +179,7 @@ class PlaintextMessage(Message):
         
         Returns: self.message_text_encrypted
         '''
-        pass #delete this line and replace with your code here
+        return self.message_text_encrypted
 
     def change_shift(self, shift):
         '''
@@ -187,8 +192,9 @@ class PlaintextMessage(Message):
 
         Returns: nothing
         '''
-        pass #delete this line and replace with your code here
-
+        self.shift = shift
+        self.encrypting_dict = Message.build_shift_dict(self, shift)
+        self.message_text_encrypted = Message.apply_shift(self, shift)
 
 class CiphertextMessage(Message):
     def __init__(self, text):
