0baa731b09bc52d0c1d84d597e64f5da36d26624
wonderui <wonderui@qq.com>
2018-02-03 23:46:30 +0800

    update

---
+++ 0_Inbox/apscheduler/sched.py
@@ -0,0 +1,15 @@
+import datetime
+import pandas as pd
+#from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
+
+list_1 = []
+def test_1():
+    list_1.append(str(datetime.datetime.now())[0:19])
+    pd.Series(list_1).to_csv('apsched.csv')
+    print('job executed!')
+    print(str(datetime.datetime.now())[0:19] + '\n')
+    
+sched = BackgroundScheduler()
+sched.add_job(test_1, 'interval', seconds=10)
+sched.start()
